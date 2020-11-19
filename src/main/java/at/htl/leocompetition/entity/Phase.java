26bdb033a546e67847b87571c52e252491747591
rosaliemandel <r.mandel@students.htl-leonding.ac.at>
2020-11-19 17:25:57 +0100

    create entity Phase #5

---
+++ src/main/java/at/htl/leocompetition/entity/Phase.java
@@ -0,0 +1,55 @@
+package at.htl.leocompetition.entity;
+
+import org.w3c.dom.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Phase {
+
+    private Long phaseId;
+    private String phaseName;
+    private List<Node> nodeList = new ArrayList<>();
+
+    public Phase(Long phaseId, String phaseName, List<Node> nodeList) {
+        this.phaseId = phaseId;
+        this.phaseName = phaseName;
+        this.nodeList = nodeList;
+    }
+
+    public Phase() {
+    }
+
+    public Long getPhaseId() {
+        return phaseId;
+    }
+
+    public void setPhaseId(Long phaseId) {
+        this.phaseId = phaseId;
+    }
+
+    public String getPhaseName() {
+        return phaseName;
+    }
+
+    public void setPhaseName(String phaseName) {
+        this.phaseName = phaseName;
+    }
+
+    public List<Node> getNodeList() {
+        return nodeList;
+    }
+
+    public void setNodeList(List<Node> nodeList) {
+        this.nodeList = nodeList;
+    }
+
+    @Override
+    public String toString() {
+        return "Phase{" +
+                "phaseId=" + phaseId +
+                ", phaseName='" + phaseName + '\'' +
+                ", nodeList=" + nodeList +
+                '}';
+    }
+}
