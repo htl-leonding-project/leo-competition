df84b5780a805ce7c9ac09122646413f79ffea9f
sandy <sandytang2406@gmail.com>
2020-11-19 17:30:33 +0100

    create entity Node #5

---
+++ src/main/java/at/htl/leocompetition/entity/Node.java
@@ -0,0 +1,60 @@
+package at.htl.leocompetition.entity;
+
+public class Node {
+    private Long nodeId;
+    private Node rightnode;
+    private Node leftNode;
+    private Node parentNode;
+
+    public Node(Long nodeId, Node rightnode, Node leftNode, Node parentNode) {
+        this.nodeId = nodeId;
+        this.rightnode = rightnode;
+        this.leftNode = leftNode;
+        this.parentNode = parentNode;
+    }
+
+    public Node() {
+    }
+
+    public Long getNodeId() {
+        return nodeId;
+    }
+
+    public void setNodeId(Long nodeId) {
+        this.nodeId = nodeId;
+    }
+
+    public Node getRightnode() {
+        return rightnode;
+    }
+
+    public void setRightnode(Node rightnode) {
+        this.rightnode = rightnode;
+    }
+
+    public Node getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(Node leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public Node getParentNode() {
+        return parentNode;
+    }
+
+    public void setParentNode(Node parentNode) {
+        this.parentNode = parentNode;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "nodeId=" + nodeId +
+                ", rightnode=" + rightnode +
+                ", leftNode=" + leftNode +
+                ", parentNode=" + parentNode +
+                '}';
+    }
+}
