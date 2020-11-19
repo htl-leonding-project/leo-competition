696a2f3c9b804ed6590c6eaabcd60040232e67c3
sandy <sandytang2406@gmail.com>
2020-11-19 17:36:37 +0100

    edit entity Node #5

--- src/main/java/at/htl/leocompetition/entity/Node.java
+++ src/main/java/at/htl/leocompetition/entity/Node.java
@@ -5,12 +5,16 @@ public class Node {
     private Node rightnode;
     private Node leftNode;
     private Node parentNode;
+    private Team team1;
+    private Team team2;
 
-    public Node(Long nodeId, Node rightnode, Node leftNode, Node parentNode) {
+    public Node(Long nodeId, Node rightnode, Node leftNode, Node parentNode, Team team1, Team team2) {
         this.nodeId = nodeId;
         this.rightnode = rightnode;
         this.leftNode = leftNode;
         this.parentNode = parentNode;
+        this.team1 = team1;
+        this.team2 = team2;
     }
 
     public Node() {
@@ -48,6 +52,22 @@ public class Node {
         this.parentNode = parentNode;
     }
 
+    public Team getTeam1() {
+        return team1;
+    }
+
+    public void setTeam1(Team team1) {
+        this.team1 = team1;
+    }
+
+    public Team getTeam2() {
+        return team2;
+    }
+
+    public void setTeam2(Team team2) {
+        this.team2 = team2;
+    }
+
     @Override
     public String toString() {
         return "Node{" +
@@ -55,6 +75,8 @@ public class Node {
                 ", rightnode=" + rightnode +
                 ", leftNode=" + leftNode +
                 ", parentNode=" + parentNode +
+                ", team1=" + team1 +
+                ", team2=" + team2 +
                 '}';
     }
 }
