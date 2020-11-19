7dbb61ccc594da58b1b5e4592b01f7ef4c4c94bf
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2020-11-19 17:37:51 +0100

    created InitBean #5

---
+++ src/main/java/at/htl/leocompetition/control/InitBean.java
@@ -0,0 +1,18 @@
+package at.htl.leocompetition.control;
+
+
+public class InitBean {
+
+    public InitBean() {
+        runPrototype();
+    }
+
+    private void runPrototype() {
+    }
+
+
+    private void init() {
+
+    }
+
+}
