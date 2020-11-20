2a3981f000ac37f825c99cff175b5e55525d3ba2
sandy <sandytang2406@gmail.com>
2020-11-20 12:36:32 +0100

    implement Match #5

---
+++ src/main/java/at/htl/leocompetition/entity/Match.java
@@ -0,0 +1,86 @@
+package at.htl.leocompetition.entity;
+
+public class Match {
+
+    private Long matchId;
+    private Team team1;
+    private Team team2;
+    private int pointsTeam1;
+    private int pointsTeam2;
+
+    public Match() {
+    }
+
+    public Match(Long matchId, Team team1, Team team2) {
+        this.matchId = matchId;
+        this.team1 = team1;
+        this.team2 = team2;
+    }
+
+    public Match(Long matchId, Team team1, Team team2, int pointsTeam1, int pointsTeam2) {
+        this.matchId = matchId;
+        this.team1 = team1;
+        this.team2 = team2;
+        this.pointsTeam1 = pointsTeam1;
+        this.pointsTeam2 = pointsTeam2;
+    }
+
+    public Long getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(Long matchId) {
+        this.matchId = matchId;
+    }
+
+    public Team getTeam1() {
+        return team1;
+    }
+
+    public void setTeam1(Team team1) {
+        this.team1 = team1;
+    }
+
+    public Team getTeam2() {
+        return team2;
+    }
+
+    public void setTeam2(Team team2) {
+        this.team2 = team2;
+    }
+
+    public int getPointsTeam1() {
+        return pointsTeam1;
+    }
+
+    public void setPointsTeam1(int pointsTeam1) {
+        this.pointsTeam1 = pointsTeam1;
+    }
+
+    public int getPointsTeam2() {
+        return pointsTeam2;
+    }
+
+    public void setPointsTeam2(int pointsTeam2) {
+        this.pointsTeam2 = pointsTeam2;
+    }
+
+    public Team getWinner() {
+        return pointsTeam1 > pointsTeam2 ? team1 : team2;
+    }
+
+    public Team getLoser() {
+        return pointsTeam1 < pointsTeam2 ? team1 : team2;
+    }
+
+    @Override
+    public String toString() {
+        return "Match{" +
+                "matchId=" + matchId +
+                ", team1=" + team1 +
+                ", team2=" + team2 +
+                ", pointsTeam1=" + pointsTeam1 +
+                ", pointsTeam2=" + pointsTeam2 +
+                '}';
+    }
+}
