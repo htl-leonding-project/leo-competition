99ce9ad22c5fcba5aacc1a5cc760906416abfbd8
rosaliemandel <r.mandel@students.htl-leonding.ac.at>
2020-11-20 10:38:58 +0100

    create PhaseTest #12

---
+++ src/test/java/at/htl/leocompetition/entity/PhaseTest.java
@@ -0,0 +1,18 @@
+package at.htl.leocompetition.entity;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PhaseTest {
+
+    @Order(10)
+    @Test
+    public void createPhase_Test(){
+
+    }
+
+}
