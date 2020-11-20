8866b80e3278b4f1737da82e0b333d9cd3331983
sandy <sandytang2406@gmail.com>
2020-11-20 11:18:25 +0100

    create toStringNode_Test #11

--- src/test/java/at/htl/leocompetition/entity/NodeTest.java
+++ src/test/java/at/htl/leocompetition/entity/NodeTest.java
@@ -17,5 +17,15 @@ public class NodeTest {
         assertThat(node).isNotNull();
     }
 
+    @Order(20)
+    @Test
+    public void toStringNode_Test(){
+        Team team1 = new Team((long) 1,"1AHITM");
+        Team team2 = new Team((long) 1,"1AHIF");
+        Node node = new Node((long) 1,null, null,null,team1,team2);
 
+        assertThat(node.toString()).isEqualTo("Node{nodeId=1, rightnode=null, " + "leftNode=null, " +
+                "parentNode=null, team1=Team{teamName='1AHITM', teamId=1}, team2=Team{teamName='1AHIF', teamId=1}}"
+        );
+    }
 }
