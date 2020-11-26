63f90296327c49a7d8a811ab6b70e56cbcc12323
rosaliemandel <r.mandel@students.htl-leonding.ac.at>
2020-11-26 11:07:41 +0100

    reformat changes #12

--- src/test/java/at/htl/leocompetition/entity/MatchTest.java
+++ src/test/java/at/htl/leocompetition/entity/MatchTest.java
@@ -10,13 +10,11 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MatchTest {
-
-
+    
     @Order(10)
     @Test
     public void createMatch_Test() {
         List<Node> nodeList = new ArrayList<>();
-
         LocalDate date = LocalDate.parse("2017-12-15");
         Competition competition = new Competition((long) 1, date);
         Phase phase = new Phase((long) 1, "phase 1", nodeList, competition);
@@ -32,7 +30,6 @@ public class MatchTest {
     @Test
     public void toString_Test() {
         List<Node> nodeList = new ArrayList<>();
-
         LocalDate date = LocalDate.parse("2017-12-15");
         Competition competition = new Competition((long) 1, date);
         Phase phase = new Phase((long) 1, "phase 1", nodeList, competition);
@@ -40,8 +37,9 @@ public class MatchTest {
         Team team2 = new Team((long) 1, "1AHIF", competition);
         Match match = new Match((long) 1, team1, team2, 2, 1, phase);
 
-        assertThat(match.toString()).isEqualTo("Match{matchId=1, team1=Team{teamId=1, teamName='1AHITM', competition=Competition{competitionId=1, date=null}}, team2=Team{teamId=1, teamName='1AHIF', competition=Competition{competitionId=1, date=null}}, pointsTeam1=2, pointsTeam2=1, phase=Phase{phaseId=1, phaseName='phase 1', nodeList=[], competition=Competition{competitionId=1, date=null}}}");
+        assertThat(match.toString()).isEqualTo("Match{matchId=1, team1=Team{teamId=1, teamName='1AHITM'," +
+                " competition=Competition{competitionId=1, date=null}}, team2=Team{teamId=1, teamName='1AHIF'," +
+                " competition=Competition{competitionId=1, date=null}}, pointsTeam1=2, pointsTeam2=1," +
+                " phase=Phase{phaseId=1, phaseName='phase 1', nodeList=[], competition=Competition{competitionId=1, date=null}}}");
     }
-
-
 }
