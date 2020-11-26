96738fa02d55d0edbb0ca803f63771a9c04906b8
sandy <sandytang2406@gmail.com>
2020-11-26 10:58:14 +0100

    add team to first competition go through #8

--- src/main/java/at/htl/leocompetition/control/InitBean.java
+++ src/main/java/at/htl/leocompetition/control/InitBean.java
@@ -6,6 +6,8 @@ import at.htl.leocompetition.entity.Node;
 import at.htl.leocompetition.entity.Phase;
 import at.htl.leocompetition.entity.Team;
 
+import java.time.LocalDate;
+
 public class InitBean {
 
     public InitBean() {
@@ -14,19 +16,18 @@ public class InitBean {
 
     private void runPrototype() {
 
-        Team team1AHEL = new Team((long) 1,"1AHEL");
-        Team team1AHIF = new Team((long) 2,"1AHIF");
-        Team team1AHITM = new Team((long) 3,"1AHITM");
-        Team team1AHBG = new Team((long) 4,"1AHBG");
-        Team team1BHEL = new Team((long) 5,"1BHEL");
-        Team team1BHIF = new Team((long) 6,"1BHIF");
-        Team team1BHITM = new Team((long) 7,"1BHITM");
-        Team team1BHBG = new Team((long) 8,"1BHBG");
+        LocalDate date = LocalDate.parse("2017-12-15");
+
+        Team team1AHEL = new Team((long) 1,"1AHEL",new Competition((long) 1,date));
+        Team team1AHIF = new Team((long) 2,"1AHIF",new Competition((long) 1,date));
+        Team team1AHITM = new Team((long) 3,"1AHITM",new Competition((long) 1,date));
+        Team team1AHBG = new Team((long) 4,"1AHBG",new Competition((long) 1,date));
+        Team team1BHEL = new Team((long) 5,"1BHEL",new Competition((long) 1,date));
+        Team team1BHIF = new Team((long) 6,"1BHIF",new Competition((long) 1,date));
+        Team team1BHITM = new Team((long) 7,"1BHITM",new Competition((long) 1,date));
+        Team team1BHBG = new Team((long) 8,"1BHBG",new Competition((long) 1,date));
 
         Competition competition = new Competition();
-
-
-
 
     }
 
